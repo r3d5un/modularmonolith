6422ea0cf25d14e2ce9b76aa66bc212a52dd4f4f
r3d5un <r3d5un@pm.me>
2024-03-27 19:41:58 +0100

    Configure application logger

--- cmd/monolith/main.go
+++ cmd/monolith/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"os"
+
+	"github.com/google/uuid"
+	"github.com/r3d5un/modularmonolith/cmd/monolith/config"
 )
 
 func main() {
@@ -13,5 +17,23 @@ func main() {
 }
 
 func run() (err error) {
+	handler := slog.NewJSONHandler(os.Stdout, nil)
+	bareLogger := slog.New(handler)
+	logger := bareLogger.With(
+		slog.Group(
+			"application_instance",
+			slog.String("instance_id", uuid.New().String()),
+		),
+	)
+	slog.SetDefault(logger)
+
+	logger.Info("loading configuration")
+	config, err := config.New()
+	if err != nil {
+		bareLogger.Error("unable to load config", "error", err)
+		return err
+	}
+	logger.Info("configuration loaded", "configuration", config)
+
 	return nil
 }
