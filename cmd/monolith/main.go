454e843d5ca3645de3e42c8c3d7da1c8eba70255
r3d5un <r3d5un@pm.me>
2024-03-27 19:22:47 +0100

    Restructure main function

--- cmd/monolith/main.go
+++ cmd/monolith/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	if err := run(); err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
+
+func run() (err error) {
+	return nil
 }
