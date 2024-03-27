5b5c16555b165d5a906095a579fd66fb8bc15090
r3d5un <r3d5un@pm.me>
2024-03-27 19:41:49 +0100

    Add function openDB

---
+++ cmd/monolith/db.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	"github.com/r3d5un/modularmonolith/cmd/monolith/config"
+)
+
+func openDB(config config.DatabaseConfiguration) (*sql.DB, error) {
+	db, err := sql.Open("sqlserver", config.DSN)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(config.MaxOpenConns)
+	db.SetMaxIdleConns(config.MaxIdleConns)
+
+	duration, err := time.ParseDuration(config.MaxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+	db.SetConnMaxIdleTime(duration)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
