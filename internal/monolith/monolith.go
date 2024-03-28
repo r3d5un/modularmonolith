b2acb347f16eed1050add6e51518b94513abae9e
r3d5un <r3d5un@pm.me>
2024-03-28 15:14:53 +0100

    Add interfaces for the Monolith and Modules

---
+++ internal/monolith/monolith.go
@@ -0,0 +1,23 @@
+package monolith
+
+import (
+	"context"
+	"database/sql"
+	"log/slog"
+	"net/http"
+
+	"github.com/r3d5un/modularmonolith/internal/config"
+	"github.com/r3d5un/modularmonolith/internal/queue"
+)
+
+type Monolith interface {
+	Config() *config.Configuration
+	Logger() *slog.Logger
+	DB() *sql.DB
+	MQ() *queue.ChannelPool
+	Mux() *http.ServeMux
+}
+
+type Module interface {
+	Startup(ctx context.Context, mono *Monolith)
+}
