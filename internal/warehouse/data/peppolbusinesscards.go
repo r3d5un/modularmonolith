b59cbd08394963807fed12777c739fc6749c1cef
r3d5un <r3d5un@pm.me>
2024-03-31 18:58:32 +0200

    Implement PeppolBusinessCardModel Get method

--- internal/warehouse/data/peppolbusinesscards.go
+++ internal/warehouse/data/peppolbusinesscards.go
@@ -3,8 +3,11 @@ package data
 import (
 	"context"
 	"database/sql"
+	"encoding/json"
+	"log/slog"
 	"time"
 
+	"github.com/r3d5un/modularmonolith/internal/logging"
 	"github.com/r3d5un/modularmonolith/internal/peppol"
 )
 
@@ -17,11 +20,48 @@ type PeppolBusinessCard struct {
 }
 
 type PeppolBusinessCardModel struct {
-	DB *sql.DB
+	DB      *sql.DB
+	Timeout *time.Duration
 }
 
 func (m *PeppolBusinessCardModel) Get(ctx context.Context, id string) (*PeppolBusinessCard, error) {
-	return nil, nil
+	logger := logging.LoggerFromContext(ctx)
+
+	stmt := `SELECT id, name, countrycode, last_updated, business_card
+FROM peppol_business_cards
+WHERE id = $1;`
+
+	qCtx, cancel := context.WithTimeout(ctx, *m.Timeout)
+	defer cancel()
+
+	var pbc PeppolBusinessCard
+	var jpbc []byte
+
+	logger.LogAttrs(
+		qCtx, slog.LevelInfo, "querying peppol business card",
+		slog.String("string", stmt), slog.String("id", id),
+	)
+	row := m.DB.QueryRowContext(qCtx, stmt, id)
+	err := row.Scan(&pbc.ID, &pbc.Name, &pbc.CountryCode, &pbc.LastUpdated, jpbc)
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			logger.LogAttrs(qCtx, slog.LevelInfo, "no record found")
+			return nil, ErrNoRecord
+		default:
+			logger.Error("unable to query business card", "error", err)
+			return nil, err
+		}
+	}
+	logger.Info("data retrieved")
+
+	err = json.Unmarshal(jpbc, &pbc.PeppolBusinessCard)
+	if err != nil {
+		logger.Error("error unmarhaling peppol business card", "error", err)
+		return nil, err
+	}
+
+	return &pbc, nil
 }
 
 func (m *PeppolBusinessCardModel) Insert(
