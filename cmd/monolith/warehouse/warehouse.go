ec22eef930596c254df9773017f0edf804a5868f
r3d5un <r3d5un@pm.me>
2024-03-31 17:07:23 +0200

    Add module logger

--- cmd/monolith/warehouse/warehouse.go
+++ cmd/monolith/warehouse/warehouse.go
@@ -2,10 +2,23 @@ package warehouse
 
 import (
 	"context"
+	"log/slog"
 
 	"github.com/r3d5un/modularmonolith/internal/monolith"
 )
 
-type Module struct{}
+type Module struct {
+	log *slog.Logger
+}
 
-func (m *Module) Startup(ctx context.Context, mono monolith.Monolith) {}
+func (m *Module) Startup(ctx context.Context, mono monolith.Monolith) {
+	m.initModuleLogger(mono.Logger())
+	m.log.Info("module logger initialized")
+
+	// TODO: Add models
+	// TODO: Add endpoints
+}
+
+func (m *Module) initModuleLogger(monoLogger *slog.Logger) {
+	m.log = monoLogger.With(slog.Group("module", slog.String("name", "warehouse")))
+}
