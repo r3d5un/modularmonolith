5beef9723ec4aab31ba3f5c888ed4810a7e89003
r3d5un <r3d5un@pm.me>
2024-04-01 02:29:48 +0200

    Register warehouse endpoint

--- cmd/monolith/warehouse/warehouse.go
+++ cmd/monolith/warehouse/warehouse.go
@@ -3,6 +3,7 @@ package warehouse
 import (
 	"context"
 	"log/slog"
+	"net/http"
 	"time"
 
 	"github.com/r3d5un/modularmonolith/internal/monolith"
@@ -22,9 +23,28 @@ func (m *Module) Startup(ctx context.Context, mono monolith.Monolith) {
 	timeout := time.Duration(mono.Config().DB.Timeout) * time.Second
 	m.models = data.NewModels(mono.DB(), &timeout)
 
-	// TODO: Add endpoints
+	m.log.Info("registering routes")
+	m.registerEndpoints(mono.Mux())
 }
 
 func (m *Module) initModuleLogger(monoLogger *slog.Logger) {
 	m.log = monoLogger.With(slog.Group("module", slog.String("name", "warehouse")))
+}
+
+type RouteDefinition struct {
+	Path    string
+	Handler http.HandlerFunc
+}
+
+type RouteDefinitionList []RouteDefinition
+
+func (m *Module) registerEndpoints(mux *http.ServeMux) {
+	routes := RouteDefinitionList{
+		{"GET /api/v1/warehouse/peppolbusinesscards", m.getPeppolBusinessCardHandler},
+	}
+
+	for _, d := range routes {
+		m.log.Info("adding route", "route", d.Path)
+		mux.Handle(d.Path, d.Handler)
+	}
 }
